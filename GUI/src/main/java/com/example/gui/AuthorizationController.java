9c80fbe35052b66752e45fcbc43f404de44c1280
ruslan savelev <carry2002life@mail.ru>
2022-04-02 21:55:08 +0300

    exit and minimize button Commit

--- GUI/src/main/java/com/example/gui/AuthorizationController.java
+++ GUI/src/main/java/com/example/gui/AuthorizationController.java
@@ -1,18 +1,37 @@
 package com.example.gui;
 
-import Client.Client;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TextField;
-
-import java.io.IOException;
+import javafx.scene.image.ImageView;
 
 public class AuthorizationController {
 
     @FXML
+    private Button exitButton;
+    @FXML
+    private Button minimizeButton;
+    @FXML
     private TextField loginField;
     @FXML
     private TextField passwordField;
+
+    @FXML
+    public void initialize(){
+        ImageView exitView = new ImageView(getClass().getResource("/assets/exit.png").toExternalForm());
+        exitView.setFitWidth(12);
+        exitView.setFitHeight(12);
+        exitButton.setGraphic(exitView);
+        exitView.setPreserveRatio(true);
+
+        ImageView minimizeView = new ImageView(getClass().getResource("/assets/minimize.png").toExternalForm());
+        minimizeView.setFitWidth(12);
+        minimizeView.setFitHeight(12);
+        minimizeButton.setGraphic(minimizeView);
+        minimizeView.setPreserveRatio(true);
+    }
 
     @FXML
     protected void cancelButtonController() {
