cd2223fcc3dd8c19a843df6afb329527f66769f1
ruslan savelev <carry2002life@mail.ru>
2022-04-02 14:05:30 +0300

    command := register

--- GUI/src/main/java/com/example/gui/Main.java
+++ GUI/src/main/java/com/example/gui/Main.java
@@ -2,33 +2,43 @@ package com.example.gui;
 
 import Client.Client;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     private TextField login;
     private TextField password;
+    @FXML
     private Button button;
+    private VBox vBox;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage)  {
         stage.setTitle("Регистрация");
         Group root = new Group();
         Scene scene = new Scene(root, 400, 300);
+        stage.setScene(scene);
 
         stage.setMaxWidth(scene.getWidth());
         stage.setMinWidth(scene.getWidth());
         stage.setMaxHeight(scene.getHeight());
         stage.setMinHeight(scene.getHeight());
-
         login = new TextField();
         login.setPrefWidth(150);
         login.setPrefHeight(20);
@@ -50,14 +60,13 @@ public class Main extends Application {
         button.setLayoutX(scene.getWidth() / 2 - password.getPrefWidth() / 2);
         button.setLayoutY(scene.getHeight() / 2);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                try {
-                    Client c = new Client();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        button.setOnAction(actionEvent -> {
+            try {
+                Client c = new Client();
+                c.sendMessage("command := register " + login.getText() + ' ' + password.getText());
+                c.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
 
